5008ca137a0e3fece87393310789fa31e078da3d
mpj5142 <mpj5142@psu.edu>
2016-02-15 16:37:43 -0500

    Modifications to make script run, i.e. add SD field and spaces between files

--- kallisto.sh
+++ kallisto.sh
@@ -2,6 +2,4 @@
 
 kallisto index -i kallisto.idx Trinity.fasta
 
-kallisto quant -i trinity_out_dir/kallisto.idx -o kallisto_out/ -l 200  -b 15 -t 5 --single Sample_1/Sample_1_trim.fastq.gz,Sample_2/Sample_2_trim.fastq.gz,Sample_3/Sample_3_trim.fastq.gz,Sample_4/Sample_4_trim.fastq.gz,Sample_5/Sample_5_trim.fastq.gz,Sample_6/Sample_6_trim.fa
-stq.gz,Sample_7/Sample_7_trim.fastq.gz,Sample_8/Sample_8_trim.fastq.gz,Sample_9/Sample_9_trim.fastq.gz,Sample_10/Sample_10_trim.fastq.gz,Sample_11/Sample_11_trim.fastq.gz,Sample_12/Sample_12_trim.fastq.gz,Sample_13/Sample_13_trim.fastq.gz,Sample_14/Sample_14_tr
-im.fastq.gz,Sample_15/Sample_15_trim.fastq.gz,Sample_16/Sample_16_trim.fastq.gz
+kallisto quant -i trinity_out_dir/kallisto.idx -o kallisto_out/ -l 200 -s 1  -b 15 -t 5 --single Sample_1/Sample_1_trim.fastq.gz Sample_2/Sample_2_trim.fastq.gz Sample_3/Sample_3_trim.fastq.gz Sample_4/Sample_4_trim.fastq.gz Sample_5/Sample_5_trim.fastq.gz Sample_6/Sample_6_trim.fastq.gz Sample_7/Sample_7_trim.fastq.gz Sample_8/Sample_8_trim.fastq.gz Sample_9/Sample_9_trim.fastq.gz Sample_10/Sample_10_trim.fastq.gz Sample_11/Sample_11_trim.fastq.gz Sample_12/Sample_12_trim.fastq.gz Sample_13/Sample_13_trim.fastq.gz Sample_14/Sample_14_trim.fastq.gz Sample_15/Sample_15_trim.fastq.gz Sample_16/Sample_16_trim.fastq.gz
