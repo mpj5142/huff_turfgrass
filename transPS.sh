e45fedfab2738879686a364c0eddc8a4a9ab0c9f
mpj5142 <mpj5142@psu.edu>
2016-02-25 15:56:14 -0500

    Update directory path

--- transPS.sh
+++ transPS.sh
@@ -4,4 +4,4 @@ makeblastdb -in transps/maize_protein.fa -dbtype prot
 
 blastx -db transps/maize_protein.fa -query trinity_reverse/Trinity.fasta -out transps/blastx.out -outfmt 6 -evalue 0.01 -max_target_seqs 20
 
-perl /home/matt/work/src/TransPS1.1.0/transps.pl -t Trinity.fasta -b transps/blastx.out
+perl /home/matt/work/src/TransPS1.1.0/transps.pl -t trinity_reverse/Trinity.fasta -b transps/blastx.out
